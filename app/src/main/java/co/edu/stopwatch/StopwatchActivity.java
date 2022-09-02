887dbe17317e84d3873e02e75c588868dfa44620
Juan Sebastián Castaño Ramírez <jscr00@hotmail.com>
2022-09-01 20:34:30 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/stopwatch/StopwatchActivity.java
@@ -0,0 +1,59 @@
+package co.edu.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    private boolean isRunning;
+    private int seconds = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        isRunning = true;
+    }
+
+    public void onClickStop(View view) {
+        isRunning=false;
+    }
+
+    public void onClickReset(View view) {
+        isRunning = false;
+        seconds=0;
+    }
+
+    private void runTimer(){
+
+        TextView timeView = (TextView) findViewById(R.id.time_view);
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int remainingSeconds = seconds%60;
+
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d",hours,minutes,remainingSeconds);
+                timeView.setText(time);
+                if(isRunning){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+}
